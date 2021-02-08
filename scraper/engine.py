09662ca2f9d322385665319bb1d044b394023e8e
deeptrial <resnick.xing@foxmail.com>
2021-02-08 14:20:56 +0800

    update engine

--- scraper/engine.py
+++ scraper/engine.py
@@ -4,44 +4,96 @@ import requests
 import urllib
 from colorama import init, Fore, Style
 from time import sleep
-import random
+import random,re,datetime
 init()
 
-def download_videos(username,save_dir,url_list):
-    if not os.path.exists(save_dir + "/" + username):
-        os.makedirs(save_dir + "/" + username)
-    save_dir = os.path.join(save_dir, username)
 
-    for i, urls in enumerate(url_list):
-        data_time, url = urls
+def download_videos(data_frame,save_dir):
+    try:
+        username = data_frame['UserName'].iloc[0]
+        if not os.path.exists(save_dir + "/" + username):
+            os.makedirs(save_dir + "/" + username)
+        save_dir = os.path.join(save_dir, username)
+    except:
+        pass
+
+    for i, url_video in enumerate(data_frame["Video link"]):
+        if len(url_video)==0:
+            continue
         try:
-            res = requests.get(url, stream=True)
+            data_time = data_frame['Timestamp'].iloc[i]
+            res = requests.get(url_video[0], stream=True)
             with open(save_dir + '/' + data_time + '_post-' + str(i + 1) + ".mp4", "wb") as mp4:
-                for chunk in res.iter_content(
-                        chunk_size=1024 * 1024):  # 当流下载时，用Response.iter_content或许更方便些。requests.get(url)默认是下载在内存中的，下载完成才存到硬盘上，可以用Response.iter_content　来边下载边存硬盘
+                for chunk in res.iter_content(chunk_size=1024 * 1024):  # 当流下载时，用Response.iter_content或许更方便些。requests.get(url)默认是下载在内存中的，下载完成才存到硬盘上，可以用Response.iter_content　来边下载边存硬盘
                     if chunk:
                         mp4.write(chunk)
         except:
-            print('\r[' + Fore.YELLOW + 'WARNING' + Style.RESET_ALL + '] ' +  url + " video download failed")
+            print('\r[' + Fore.YELLOW + 'WARNING' + Style.RESET_ALL + '] ' +  url_video + " video download failed")
+        sleep(random.uniform(0, 1))
 
-        sleep(random.randint(0, 2))
 
-
-def download_images(username,save_dir,url_list):
+def download_images(data_frame, save_dir):
+    username = data_frame['UserName'].iloc[0]
     if not os.path.exists(save_dir + "/" + username):
         os.makedirs(save_dir + "/" + username)
     save_dir = os.path.join(save_dir, username)
 
-    for i, urls in enumerate(url_list):
-        data_time,url=urls
-        try:
-            urllib.request.urlretrieve(url, save_dir + '/'+data_time+'_post-'+str(i+ 1) + ".jpg")
-        except:
+    for i, url_v in enumerate(data_frame["Image link"]):
+        for j, url in enumerate(url_v):
+            data_time=data_frame['Timestamp'].iloc[i]
             try:
-                imgres = requests.get(url)
-                with open(save_dir + '/' + data_time + '_post-' + str(i + 1) + ".jpg", "wb") as f:
-                    f.write(imgres.content)
+                urllib.request.urlretrieve(url, save_dir + '/'+data_time+'_post-'+str(i+ 1)+"-"+str(j+1) + ".jpg")
             except:
-                print('\r[' + Fore.YELLOW + 'WARNING' + Style.RESET_ALL + '] ' + url + " image download error")
+                try:
+                    imgres = requests.get(url)
+                    with open(save_dir + '/' + data_time + '_post-' + str(i + 1) + ".jpg", "wb") as f:
+                        f.write(imgres.content)
+                except:
+                    print('\r[' + Fore.YELLOW + 'WARNING' + Style.RESET_ALL + '] ' + url + " image download error")
+            sleep(random.uniform(0,1))
 
-        sleep(random.randint(0,2))
+def parse_time(time_str):
+
+    month_convert={"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
+
+    time_split=time_str.split(" ")
+    month=time_split[1]
+    day=time_split[2]
+    year=time_split[-1]
+
+    time=str(year)+"-"+str(month_convert[month])+"-"+str(day)
+    return datetime.datetime.strptime(time, '%Y-%m-%d')
+
+
+def filter_text(text):
+    re_tag = re.compile('</?\w+[^>]*>')  # HTML标签
+    new_text = re.sub(re_tag, '', text)
+    new_text = re.sub(",+", ",", new_text)  # 合并逗号
+    new_text = re.sub(" +", " ", new_text)  # 合并空格
+    new_text = re.sub("[...|…|。。。]+", "...", new_text)  # 合并句号
+    new_text = re.sub("-+", "--", new_text)  # 合并-
+    new_text = re.sub("———+", "———", new_text)  # 合并-
+    return new_text
+
+def parse_blog(mblog):
+    current_time = mblog['mblog']['created_at']
+    current_date = parse_time(current_time)
+    username=mblog['mblog']['user']['screen_name']
+    user_id = mblog['mblog']['user']['id']
+    reposts=mblog['mblog']['reposts_count']
+    comments=mblog['mblog']['comments_count']
+    likes=mblog['mblog']['attitudes_count']
+    text = filter_text(mblog['mblog']['text'])
+    images=[]
+    videos=[]
+
+    img_url_list = mblog['mblog']['pic_ids']
+    if len(img_url_list):
+        img_prefix = mblog['mblog']['original_pic'].split("large")[0]
+        for url in img_url_list:
+            images.append(img_prefix + "large/" + url + ".jpg")
+
+    if r"微博视频" in text:
+        video_url = mblog['mblog']["page_info"]["urls"]['mp4_ld_mp4']
+        videos.append(video_url)
+    return current_date,[username,user_id,current_date.strftime("%Y-%m-%d %H:%M:%S").split(" ")[0],text,reposts,comments,likes,images,videos]
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,31 +1,66 @@
 from weibo_scraper import get_weibo_tweets_by_name
 import datetime,re
-from .engine import download_images,download_videos
+from .engine import download_images,download_videos,parse_blog
 import pandas as pd
 import os
 
-def parse_time(time_str):
+def merge_csv(from_account,file_list,start_date,end_date):
+    csv_path = r'./csv_log/'  # 要拼接的文件夹及其完整路径，注意不要包含中文
 
-    month_convert={"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
+    # 将该文件夹下的所有文件名存入一个列表
+    merge_file_name=from_account+"_"+start_date+"_"+end_date+".csv"
+    if file_list:
+        df = pd.read_csv(file_list[0])  # 编码默认UTF-8，若乱码自行更改
 
-    time_split=time_str.split(" ")
-    month=time_split[1]
-    day=time_split[2]
-    year=time_split[-1]
+    # 将读取的第一个CSV文件写入合并后的文件保存
+        os.remove(file_list[0])
+        df.to_csv(csv_path + '/' + merge_file_name, encoding="utf_8_sig", index=False)
 
-    time=str(year)+"-"+str(month_convert[month])+"-"+str(day)
-    return datetime.datetime.strptime(time, '%Y-%m-%d')
+    # 循环遍历列表中各个CSV文件名，并追加到合并后的文件
+        for i in range(1, len(file_list)):
+            df = pd.read_csv(file_list[i])
+            df.to_csv(csv_path + '/' + merge_file_name, encoding="utf_8_sig", index=False, header=False, mode='a+')
+            os.remove(file_list[i])
 
 
-def filter_text(text):
-    re_tag = re.compile('</?\w+[^>]*>')  # HTML标签
-    new_text = re.sub(re_tag, '', text)
-    new_text = re.sub(",+", ",", new_text)  # 合并逗号
-    new_text = re.sub(" +", " ", new_text)  # 合并空格
-    new_text = re.sub("[...|…|。。。]+", "...", new_text)  # 合并句号
-    new_text = re.sub("-+", "--", new_text)  # 合并-
-    new_text = re.sub("———+", "———", new_text)  # 合并-
-    return new_text
+def scrap_interval(
+    account_name,
+    start_date,
+    end_date,
+    save_images=False,
+    save_videos=False,
+):
+    csv_name=account_name+"_"+start_date.strftime("%Y-%m-%d")+"_"+end_date.strftime("%Y-%m-%d")+".csv"
+    data=[]
+    print("\r[INFO] 开始查询...",end="")
+    #start_date=datetime.datetime.strptime(start_date, '%Y-%m-%d')# + datetime.timedelta(days=interval)
+    #end_date=datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+    if end_date<start_date:
+        return
+    print("\r[INFO] 搜索时间段：%s-%s" %(start_date.strftime("%Y-%m-%d"),end_date.strftime("%Y-%m-%d")), end="")
+    for tweet in get_weibo_tweets_by_name(name=account_name, pages=None):
+        current_date,blog_content=parse_blog(tweet)
+        if current_date>=start_date and current_date<=end_date:
+            data.append(blog_content)
+        elif 'title' in tweet['mblog'].keys() and '置顶' in tweet['mblog']['title']['text']:
+            continue
+        elif current_date<=start_date:
+            break
+
+    data_df = pd.DataFrame(data, columns=['UserName', 'UserID','Timestamp', 'Text','Repost','Comment','Like','Image link','Video link'])
+    if not os.path.exists("./csv_log/" ):
+        os.makedirs("./csv_log/" )
+    data_df.to_csv("./csv_log/"+csv_name, encoding="utf-8-sig",index=None)
+
+    print("\r[INFO] 图片下载中",end="")
+    if len(data)>0 and save_images:
+        download_images(data_df,"./media/")
+
+    print("\r[INFO] 视频下载中",end="")
+    if len(data)>0 and save_videos:
+        download_videos(data_df,"./media/")
+    return "./csv_log/"+csv_name
 
 
 def scrap(
@@ -35,47 +70,31 @@ def scrap(
     save_images=False,
     save_videos=False,
 ):
+    init_date = start_date
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')  # + datetime.timedelta(days=interval)
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
 
-    images=[]
-    videos=[]
-    data=[]
+    csv_path = []
 
-    start_date=datetime.datetime.strptime(start_date, '%Y-%m-%d')# + datetime.timedelta(days=interval)
-    end_date=datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    while start_date + datetime.timedelta(days=60) <= end_date:
+        log_name = scrap_interval(
+            account_name=account_name,
+            start_date=start_date,
+            end_date=start_date + datetime.timedelta(days=60),
+            save_images=save_images,
+            save_videos=save_videos
+        )
+        csv_path.append(log_name)
+        start_date = start_date + datetime.timedelta(days=60)
 
-    if end_date<start_date:
-        return
+    if start_date < end_date:
+        log_name = scrap_interval(
+            account_name=account_name,
+            start_date=start_date,
+            end_date=end_date,
+            save_images=save_images,
+            save_videos=save_videos
+        )
+        csv_path.append(log_name)
 
-
-    for tweet in get_weibo_tweets_by_name(name=account_name, pages=None):
-        current_time=tweet['mblog']['created_at']
-        current_date=parse_time(current_time)
-
-        if current_date>=start_date and current_date<=end_date:
-            text=tweet['mblog']['text']
-            img_url_list=tweet['mblog']['pic_ids']
-            if len(img_url_list):
-                img_prefix=tweet['mblog']['original_pic'].split("large")[0]
-                for url in img_url_list:
-                    images.append((current_date.strftime("%Y-%m-%d %H:%M:%S").split(" ")[0],img_prefix+"large/"+url+".jpg"))
-            if r"微博视频" in text:
-                video_url=tweet['mblog']["page_info"]["urls"]['mp4_ld_mp4']
-                videos.append((current_date.strftime("%Y-%m-%d %H:%M:%S").split(" ")[0],video_url))
-            data.append((account_name,current_time,filter_text(text)))
-
-        elif 'title' in tweet['mblog'].keys() and '置顶' in tweet['mblog']['title']['text']:
-            continue
-        elif current_date<=start_date:
-            break
-    data = pd.DataFrame(data, columns=['UserName', 'Timestamp', 'Text'])
-    if not os.path.exists("./csv_log/" ):
-        os.makedirs("./csv_log/" )
-    data.to_csv("./csv_log/"+account_name+"_"+start_date.strftime("%Y-%m-%d %H:%M:%S").split(" ")[0]+"_"+end_date.strftime("%Y-%m-%d %H:%M:%S").split(" ")[0]+".csv", encoding="utf-8-sig",index=None)
-
-    print("\r[INFO] 图片下载中",end="")
-    if save_images:
-        download_images(account_name,"./media/",images)
-
-    print("\r[INFO] 视频下载中",end="")
-    if save_videos:
-        download_videos(account_name,"./media/",videos)
+    merge_csv(account_name, csv_path, init_date, end_date.strftime("%Y-%m-%d"))
